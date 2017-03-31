25fc3ae58cfe26617def882b81b959b7541e6c64
gbernas <gbernas@API-LT-022>
2017-03-31 23:13:58 +0100

    students refactored

--- finalProject/CollegeEquipmentControl.cs
+++ finalProject/CollegeEquipmentControl.cs
@@ -21,7 +21,7 @@ namespace finalProject
             Console.Clear();
             DisplayMainMenu();
 
-            var option = (MainProjectOptionEnum)GetIntegerOptionFromUser(1, 6);
+            var option = (MainProjectOptionEnum)Helpers.GetIntegerOptionFromUser(1, 6);
 
             switch (option)
             {
@@ -169,32 +169,7 @@ namespace finalProject
             }
         }
 
-        public int GetIntegerOptionFromUser(int minValue, int maxValues)
-        {
-            var userInput = Console.ReadLine();
-            var retry = true;
-            int option;
-            //chec user input
-            while (retry)
-            {
-                var parseResult = int.TryParse(userInput, out option);
-                if (parseResult)
-                {
-                    //check min max as inpru is valid
-                    if (minValue <= option && option <= maxValues)
-                    {
-                        retry = false; // return terminate loop
-                        return option;
-                    }
-                }
-
-                Console.WriteLine("Please provide valid input.");
-                userInput = Console.ReadLine();
-            }
-
-            throw new IndexOutOfRangeException();
-        }
-
+      
         private static void DisplayRentalMenu()
         {
             Console.WriteLine("****Booking******");
---
+++ finalProject/Helpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalProject
+{
+   public static class Helpers
+    {
+
+        public static int GetIntegerOptionFromUser(int minValue, int maxValues)
+        {
+            var userInput = Console.ReadLine();
+            var retry = true;
+            int option;
+            //chec user input
+            while (retry)
+            {
+                var parseResult = int.TryParse(userInput, out option);
+                if (parseResult)
+                {
+                    //check min max as inpru is valid
+                    if (minValue <= option && option <= maxValues)
+                    {
+                        retry = false; // return terminate loop
+                        return option;
+                    }
+                }
+
+                Console.WriteLine("Please provide valid input.");
+                userInput = Console.ReadLine();
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+
+    }
+}
