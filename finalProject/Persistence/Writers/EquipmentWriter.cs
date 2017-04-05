5fe6a1167b29cefaa1437782f61425f505bd985a
sirfrancline <sirfrancline@yahoo.com>
2017-04-05 22:37:05 +0100

    writer and added returned date

---
+++ finalProject/Persistence/Writers/EquipmentWriter.cs
@@ -0,0 +1,65 @@
+﻿using finalProject.Persistence.FileSingleAccessControlObjects;
+using finalProject.Persistence.Readers;
+using System.Collections.Generic;
+using System.IO;
+
+namespace finalProject.Persistence.Writers
+{
+    public class EquipmentWriter
+    {
+        private EquipmentReader reader = new EquipmentReader();
+
+        private string _fileName;
+
+        /// <summary>
+        /// CONSTRUCTOR
+        /// </summary>
+        /// <param name="fileName"></param>
+        public EquipmentWriter(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public void Add(Equipment item)
+        {
+            var list = reader.GetEquipment(_fileName);
+            list.Add(item.ID, item);
+            Save(list, true);
+        }
+
+        public void Edit(Equipment item)
+        {
+            var list = reader.GetEquipment(_fileName);
+            if (list.ContainsKey(item.ID))
+            {
+                list[item.ID] = item; // replace full item in dictionary
+                Save(list);
+            }
+        }
+
+        public void Delete(Equipment item)
+        {
+            var list = reader.GetEquipment(_fileName);
+            if (list.ContainsKey(item.ID))
+            {
+                list.Remove(item.ID);
+                Save(list);
+            }
+        }
+
+        private void Save(Dictionary<string, Equipment> list, bool isAppend = false)
+        {
+            lock (LockObjects.EqLocker)
+            {
+                using (var file = new StreamWriter(_fileName, isAppend)) //false  => overwrite
+                {
+                    foreach (var item in list)
+                    {
+                        var line = $"{item.Value.ID},{item.Value.Type},{item.Value.MaxRentalDays},{item.Value.Description}";
+                        file.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ finalProject/Persistence/Writers/StaffReader.cs
@@ -0,0 +1,48 @@
+﻿using finalProject.Persistence.FileSingleAccessControlObjects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalProject.Persistence.Writers
+{
+    public class StaffWriter
+    {
+
+        public Dictionary<string, Staff> GetStuffMembers(string fileName)
+        {
+            var staffList = new Dictionary<string, Staff>();
+            lock (LockObjects.StaffLocker)
+            {
+                if (File.Exists(fileName))
+                {
+                    using (var staffFile = new StreamReader(fileName))
+                    {
+                        while (!staffFile.EndOfStream)
+                        {
+                            var staffS = staffFile.ReadLine();
+                            var staffdetails = staffS.Split(' ');
+                            var staff = new Staff();
+                            staff.IdNumber = staffdetails[0];
+                            staff.FirstName = staffdetails[1];
+                            staff.Surname = staffdetails[2];
+                            staff.Username = staffdetails[3];
+                            staff.Password = staffdetails[4];
+                            staffList.Add(staff.IdNumber, staff);
+                        }
+                    }
+                }
+                else
+                {
+                    // missing file,
+                    Console.WriteLine("files is missing....");
+                    Console.ReadLine();
+                }
+                return staffList;
+            }
+        }
+
+    }
+}
